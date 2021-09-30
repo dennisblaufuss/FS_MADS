7a52df3dab8ce4e2d5f94c75739749c4dc2e215f
dennisblaufuss <dennis_blaufuss@web.de>
2021-09-30 22:55:22 +0200

    last changes & cleaning

--- django_debugged_assignment_2.py
+++ django_debugged_assignment_2.py
@@ -6,16 +6,16 @@ import pandas as pd
 
 class Node:
     def __init__(self, data=None):
-        self.data = data  # Assign data
-        self.next = None  # Initialize with next empty pointer
+        self.data = data
+        self.next = None
 
     def __str__(self):
         return str(self.data)
 
 
 class LinkedList:
-    def __init__(self, data=None):  # Data can be None type as well to create empty list
-        # Tail is used in a few functions for faster result
+    def __init__(self, data=None):
+        # Tail is used in 'add' functions for faster result
         self.tail = self.head = Node(data)
         self.size = 1
 
@@ -55,6 +55,14 @@ class LinkedList:
         current_index = 0
         new_node = Node(data)
         current = self.head
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return
+        if index == self.size:
+            self.add(data)
+            return
         while current.next is not None:
             last = current
             current = current.next
@@ -78,7 +86,8 @@ class LinkedList:
             current = current.next
         if current.next is self.tail:
             self.tail = current
-        current.next = current.next.next
+        else:
+            current.next = current.next.next
         self.size -= 1
         return
 
@@ -108,40 +117,40 @@ class LinkedList:
             return data[:-4]
         else:
             return "negative step is only possible in double linked list"
+            # it is possible though highly inefficient (just go for dll in that case)
 
 
-min_l = 10000  # <--- creation of the linked list
-n = 600000  # <--- quantity to insert
+# Input data
+min_l = 10000  # <--- min linked list length
+n = 600000  # <--- times a random object will be inserted at index 0
 max_l = 20000000  # <--- max linked list length
-reps = 10  # <--- number of times the 'at' gets repeated to get an average time
+reps = 10  # <--- times the 'at' function will be repeated
 
+# Creating data
 data_count = []
 data_time1 = []
 data_time2 = []
-
 while min_l <= max_l:
     new_lst = LinkedList()
-
     for j in (range(min_l)):
         new_lst.add(j)
-
+    # timing the accessing
     time_start1 = perf_counter_ns()
-    for k in range(reps):
+    for j in range(reps):
         new_lst.at(randrange(min_l))
     time_end1 = perf_counter_ns()
     time_span1 = time_end1 - time_start1
     time_in_sec_1 = (time_span1 / 1000000000)
-    data_count.append(min_l)
+    # timing the inserting
     data_time1.append(time_in_sec_1)
-
     time_start2 = perf_counter_ns()
     for j in range(n):
-        new_lst.insert(1, j)
-        j += 1
+        new_lst.insert(0, j)
     time_end2 = perf_counter_ns()
     time_span2 = time_end2 - time_start2
     time_in_sec_2 = time_span2 / 1000000000
     data_time2.append(time_in_sec_2)
+    data_count.append(min_l)
     min_l *= 2
 
 # Dataframe
@@ -152,22 +161,19 @@ df = pd.DataFrame(data)
 # Plot
 plt.figure(figsize=(16, 5))
 plt.style.use("ggplot")
-
-plt.plot(df["count"]/1000, df["time (access)"],
+plt.plot(df["count"]/1000000, df["time (access)"],
          marker="o",
          color="red",
          label="time to access " + str(reps) + " random elements")
-
-plt.plot(df["count"], df["time (insert)"],
+plt.plot(df["count"]/1000000, df["time (insert)"],
          marker="o",
          color="blue",
-         label="time to insert " + str(n) + " elements at position zero")
-
+         label="time to insert " + str(int(n/1000)) + "K elements at position zero")
 plt.legend()
 # Labeling and aligning the axes to 0
-plt.xlabel("length of linked list in thousands")
+plt.xlabel("length of linked list in millions")
 plt.xlim(xmin=0)
 plt.ylabel("time span in seconds")
 plt.ylim(ymin=0)
-plt.title("Comparison between insert at index 0 and acces of random node in linked list")
+plt.title("Comparison between inserting at index 0 and accessing of random nodes in linked list")
 plt.show()
